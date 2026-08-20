099368be7d5a2b0d9374fe6adda0ba3f86c97f66
parmis <parmisvosoghi1235@gmail.com>
2026-08-19 23:36:19 -0700

    delete

--- project/backend/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("products", views.ProductListView.as_view(), name="product-list"),
-    path("products/<int:pk>", views.ProductDetailView.as_view(), name="product-detail"),
-    path("variants/<int:pk>", views.VariantDeleteView.as_view(), name="variant-delete"),
-]
